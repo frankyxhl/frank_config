d5dc0f6f956aef54b29e8c3ed2b99eac8314d0ca
franky_xhl <franky.xhl@gmail.com>
2012-03-02 00:57:53 -0800

    add ln -s

--- init_exec.sh
+++ init_exec.sh
@@ -5,6 +5,7 @@
 
 sudo apt-get install vim
 find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;
-
-
-
+ln -s .emacs ~/.emacs
+ln -s .emacs.d ~/.emacs.d 
+ln -s .tmux.conf ~/.tmux.conf
+ln -s .zshrc ~/.zshrc
