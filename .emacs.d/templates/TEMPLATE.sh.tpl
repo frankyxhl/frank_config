a2792480a970dec518f03e8703cc5cf06ef970d6
Frank_Xu <franky.xhl@gmail.com>
2012-03-02 13:25:32 +0800

    add init_exec.sh and template sh.tpl of emacs

---
+++ .emacs.d/templates/TEMPLATE.sh.tpl
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+# -*- coding: utf-8 -*-
+# @date: (>>>ISO_DATE<<<)
+# @author: Frank.Xu
+
+
+
+
+
---
+++ init_exec.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+# -*- coding: utf-8 -*-
+# @date: 2012-03-02
+# @author: Frank.Xu
+
+sudo apt-get install vim
+find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;
+
+
+
