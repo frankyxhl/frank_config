aa23da4d70228eae83ed71faddcdcb5b7dfcc3be
Frank_Xu <franky.xhl@gmail.com>
2012-03-02 17:24:27 +0800

    del null line in init_exec.sh

--- init_exec.sh
+++ init_exec.sh
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # @date: 2012-03-02
 # @author: Frank.Xu
-
 sudo apt-get install vim
 find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;
 ln -s ~/frank_config/.emacs ~/.emacs
