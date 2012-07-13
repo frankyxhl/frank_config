bb84a37614418cfafb4cd1586f792968e9a4a184
franky_xhl <franky.xhl@gmail.com>
2012-07-13 17:09:34 +0800

    add apt-get install tmux pymacs , cololr-theme-6.6 and .fonts folder

--- init_exec.sh
+++ init_exec.sh
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @date: 2012-03-02
 # @author: Frank.Xu
-sudo apt-get install vim-nox
+sudo apt-get install vim-nox tmux pymacs 
 cd && git clone https://github.com/robbyrussell/oh-my-zsh.git
 mv oh-my-zsh .oh-my-zsh
 find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;
@@ -10,3 +10,4 @@ ln -s ~/frank_config/.emacs ~/.emacs
 ln -s ~/frank_config/.emacs.d ~/.emacs.d
 ln -s ~/frank_config/.tmux.conf ~/.tmux.conf
 ln -s ~/frank_config/.zshrc ~/.zshrc
+cp -r .fonts ~/
