b9460459364a88465a541a4bf6192e24f81716f4
Frank_Xu <franky.xhl@gmail.com>
2013-06-08 04:16:25 +0800

    rm vi color theme

--- .vimrc
+++ .vimrc
@@ -1,8 +1,4 @@
 set hlsearch
 set number
 syntax on
-colorscheme evening
 imap jj <ESC>
-
-"http://vimcolorschemetest.googlecode.com/svn/html/index-c.html"
-source ~/.vim/256-jungle.vim
--- init_exec.sh
+++ init_exec.sh
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 # @date: 2012-03-02
 # @author: Frank.Xu
-sudo apt-get install vim-nox tmux zsh 
+#sudo apt-get install vim-nox tmux zsh 
+#find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;
 cd && git clone https://github.com/robbyrussell/oh-my-zsh.git
 mv oh-my-zsh .oh-my-zsh
-find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;
 ln -s ~/frank_config/.emacs ~/.emacs
 ln -s ~/frank_config/.emacs.d ~/.emacs.d
 ln -s ~/frank_config/.tmux.conf ~/.tmux.conf
@@ -13,4 +13,4 @@ ln -s ~/frank_config/.zshrc ~/.zshrc
 ln -s ~/frank_config/.vimrc ~/.vimrc
 ln -s ~/frank_config/.vim ~/.vim
 ln -s ~/frank_config/.emacsclient ~/.emacsclient
-cp -r ~/.fonts ~/
+#cp -r ~/.fonts ~/
