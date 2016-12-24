c643b29588a51cef10a64125cd5aec03c5f610da
Frank Xu <franky.xhl@gmail.com>
2016-12-24 21:53:47 +0900

    Modify config for new macbook pro

---
+++ macos/init.sh
@@ -0,0 +1,9 @@
+brew install autojump  python
+# Use Python2's pip 
+pip install percol
+
+# Slate
+# https://github.com/jigish/slate
+# Enable Slate in Mac: http://mizage.com/help/accessibility.html
+ln -s ~/frank_config/macos/.slate ~/.slate
+ln -s ~/frank_config/macos/.slate.js ~/.slate.js
