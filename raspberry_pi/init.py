eddcf7b6956f1e51c019d20f9793a1f30928c79c
Frank Xu <franky.xhl@gmail.com>
2017-09-20 01:21:47 +0900

    Add Raspberry Init Install Script

---
+++ raspberry_pi/init.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @date: 2017-09-16
+# @author: Frank.Xu
+import os, sys
+
+def is_python3():
+    print("Checking Python3")
+    if sys.version_info < (3, 0):
+        sys.stdout.write("Sorry, requires Python 3.x\n")
+        sys.exit(1)
+
+def install():
+    print_and_cmd_list = [
+        ("Installing packages..."            ,["sudo apt-get install -y vim tmux zsh"]),
+        ("Use Vim's visul mode ..."          ,["find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;"]),
+        # sudo apt-get install -y autojump python2.7 python-pip
+        ("Installing oh-my-zsh.."            ,["cd && git clone https://github.com/robbyrussell/oh-my-zsh.git -o .oh-my-zsh"]),
+        ("Installing zsh-autosuggestions..." ,["git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions"]),
+        # https://github.com/mooz/percol
+        ("Installing percol"                 ,["sudo pip install percol"]),
+        ("Install custom themes"             ,["mkdir -p ~/.oh-my-zsh/custom/themes/",
+                                               "ln -s ~/frank_config/bira-no-ruby.zsh-theme ~/.oh-my-zsh/custom/themes/bira-no-ruby.zsh-theme"]),
+        ("Install Tmux plugin",              ["git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"])
+        # TODO check below
+        # ?? # print_then_run("tmux source ~/.tmux.conf")
+    ]
+    for (show_message, cmd_list) in print_and_cmd_list:
+        print(show_message)
+        for cmd in cmd_list:
+            print(cmd)
+            if os.system(cmd) != 0:
+                print("Error happens in cmd: {}".format(cmd))
+                sys.exit(1)
+
+
+def link():
+    print("Linking dot files to user home path...")
+    linking_name_list = [
+        ".emacs",
+        ".emacs.d",
+        ".tmux.conf",
+        ".zshrc",
+        ".zshrc.d",
+        ".vimrc",
+        ".vim",
+        ".emacsclient"
+    ]
+    for name in linking_name_list:
+        cmd = "ln -s ~/frank_config/{} ~/{}".format(name)
+        print(cmd)
+        os.system(cmd)
+
+
+if __name__ == "__main__":
+    is_python3()
+    install()
+    link()
