dc87d41810cc7159be99a3390ddd2ef2a1522f6d
Frank Xu <franky.xhl@gmail.com>
2017-09-20 01:30:40 +0900

    Add remove script for Pi

---
+++ raspberry_pi/remove_useless_stuff_in_raspberry_pi.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+# -*- coding: utf-8 -*-
+# @date: 2012-09-20
+# @author: Frank.Xu
+# @version: 0.1
+# @description: Remove not useless packages in Pi
+
+sudo apt-get remove --purge wolfram-engine libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
