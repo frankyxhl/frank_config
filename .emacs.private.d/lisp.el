1ad7b1e4f1347ef0f8b55aa4aaee45a293c93e2f
Frank Xu <franky.xhl@gmail.com>
2017-10-12 12:33:15 +0900

    Improve lisp.el

--- .emacs.private.d/lisp.el
+++ .emacs.private.d/lisp.el
@@ -1,22 +1,21 @@
 ;; fast-paren-mode
-(load-file "~/.emacs.private.d/fast-paren-mode.el")
+(load "fast-paren-mode")
 (require 'fast-paren-mode)
-
-(add-hook 'lisp-mode-hook  'turn-on-fast-paren-mode)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-fast-paren-mode)
-(add-hook 'ielm-mode-hook  'turn-on-fast-paren-mode)
-(add-hook 'lisp-interaction-mode-hook 'turn-on-fast-paren-mode)
-(add-hook 'slime-repl-mode-hook       'turn-on-fast-paren-mode)
-(add-hook 'hy-mode-hook  'turn-on-fast-paren-mode)
-
-
-(add-hook 'hy-mode-hook 'paredit-mode)
-(add-hook 'hy-mode-hook 'parinfer-mode)
-
-
-(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-(add-hook 'emacs-lisp-mode-hook 'parinfer-mode)
-
 
 (smartparens-global-mode 1)
 (global-paren-face-mode 1)
+
+
+;; lisp mode
+(add-hook 'lisp-mode-hook  'turn-on-fast-paren-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-fast-paren-mode)
+
+;; Hy mode
+(add-hook 'hy-mode-hook 'turn-on-fast-paren-mode)
+(add-hook 'hy-mode-hook 'paredit-mode)
+(add-hook 'hy-mode-hook 'parinfer-mode)
+
+;; Emacs Lisp mode
+(add-hook 'emacs-lisp-mode-hook 'turn-on-fast-paren-mode)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'parinfer-mode)
