5f1fd33d390e0db5f812bc01be39700203a37de0
Frank Xu <franky.xhl@gmail.com>
2018-04-14 14:56:56 +0900

    Add inf-clojure mode

--- .emacs.private.d/lisp.el
+++ .emacs.private.d/lisp.el
@@ -5,12 +5,14 @@
 (smartparens-global-mode 1)
 (global-paren-face-mode 1)
 
+;; clojure-mode
+(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
 
-;; lisp mode
+;; lisp-mode
 (add-hook 'lisp-mode-hook  'turn-on-fast-paren-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-fast-paren-mode)
 
-;; Hy mode
+;; hy-mode
 (add-to-list 'auto-mode-alist '("\\.hy\\'" . hy-mode))
 (add-to-list 'interpreter-mode-alist '("hy" . hy-mode))
 
@@ -18,7 +20,7 @@
 (add-hook 'hy-mode-hook 'paredit-mode)
 (add-hook 'hy-mode-hook 'parinfer-mode)
 
-;; Emacs Lisp mode
+;; emacs-lisp-mode
 (add-hook 'emacs-lisp-mode-hook 'turn-on-fast-paren-mode)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'parinfer-mode)
