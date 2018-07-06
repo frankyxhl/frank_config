130a7f0e736fd254e33ba4125ad2d2efd7f8ff7b
Frank Xu <franky.xhl@gmail.com>
2018-07-06 23:32:21 +0900

    Add git-tree command

--- .zshrc.d/git.sh
+++ .zshrc.d/git.sh
@@ -4,6 +4,9 @@
 alias git-root='cd $(git rev-parse --show-cdup)'
 alias git_diff_with_last_version="git icdiff HEAD^ HEAD"
 
+# https://stackoverflow.com/questions/5361019/viewing-full-version-tree-in-git
+alias git-tree="git log --pretty=oneline --graph --decorate --all"
+
 
 
 # https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor#answer-36573710
