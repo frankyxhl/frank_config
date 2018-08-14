92086ba4d9f858464137b2479f8f5f27c428e903
Frank Xu <franky.xhl@gmail.com>
2018-08-14 15:48:26 +0900

    Add echo in ubuntu/init.sh

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -4,17 +4,9 @@
 # @date: 2018-07-10
 # @author: Frank Xu
 sudo true
-sudo apt-get install -y git vim tmux zsh
+sudo apt-get install -y git vim tmux zsh autojump
 find /usr/share/vim/ -name 'less.sh' -exec sudo cp {} /usr/local/bin/vless \;
 # sudo apt-get install -y autojump python2.7 python-pip
-
-if [! -d ~/.oh-my-zsh ]; then
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-fi
-
-if [! -d ~/.zsh/zsh-autosuggestions ]; then
-    git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-fi
 
 
 # https://github.com/mooz/percol
@@ -23,6 +15,7 @@ fi
 
 #ln -s ~/frank_config/.emacs ~/.emacs
 #ln -s ~/frank_config/.emacs.d ~/.emacs.d
+echo 'configure .zshrc'
 if [ ! -f ~/.zshrc.js ]; then
     ln -s ~/frank_config/.zshrc ~/.zshrc
 fi
@@ -31,6 +24,7 @@ if [ ! -f ~/.zshrc.d ]; then
     ln -s ~/frank_config/.zshrc.d ~/.zshrc.d
 fi
 
+echo 'configure .vimrc'
 if [ ! -f  ~/.vimrc ]; then
     ln -s ~/frank_config/.vimrc ~/.vimrc
 fi
@@ -39,24 +33,36 @@ if [ ! -f ~/~/.vim ]; then
     ln -s ~/frank_config/.vim ~/.vim
 fi
 
+echo 'configure .emacs'
 if [ ! -f  ~/.emacsclient ]; then
     ln -s ~/frank_config/.emacsclient ~/.emacsclient
 fi
 
+echo 'configure tmux'
 if [ ! -f  ~/.tmux.conf ]; then
     ln -s ~/frank_config/.tmux.conf ~/.tmux.conf
+fi
+
+echo 'configure oh-my-zsh'
+if [! -d ~/.oh-my-zsh ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+if [! -d ~/.zsh/zsh-autosuggestions ]; then
+    git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 fi
 
 if [! -d ~/.oh-my-zsh/custom/themes ]; then
     mkdir -p ~/.oh-my-zsh/custom/themes
 fi
 
+echo 'configure oh-my-zsh custom theme'
 if [ ! -f  ~/.oh-my-zsh/custom/themes/bira-no-ruby.zsh-theme ]; then
     ln -s ~/frank_config/bira-no-ruby.zsh-theme ~/.oh-my-zsh/custom/themes/bira-no-ruby.zsh-theme
 fi
 #cp -r ~/.fonts ~/
 
-# Tmux
+echo 'configure Tmux'
 if [! -d ~/.tmux/plugins ]; then
     mkdir -p ~/.tmux/plugins
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -64,4 +70,5 @@ if [! -d ~/.tmux/plugins ]; then
 fi
 
 # Set zsh as default shell
+echo 'change default sh to zsh'
 sudo chsh -s $(which zsh)
