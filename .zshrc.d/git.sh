e750763bf7fcd7cdebf7342f04a8c5dbc25f7069
Frank Xu <franky.xhl@gmail.com>
2018-09-12 16:44:45 +0900

    Improve zshrc's git command

--- .zshrc.d/git.sh
+++ .zshrc.d/git.sh
@@ -2,15 +2,16 @@
 
 # goto current git project root path
 alias git-root='cd $(git rev-parse --show-cdup)'
-alias git_diff_with_last_version="git icdiff HEAD^ HEAD"
+alias git-diff-with-last-version="git icdiff HEAD^ HEAD"
 
 # https://stackoverflow.com/questions/5361019/viewing-full-version-tree-in-git
-alias git-tree="git log --pretty=oneline --graph --decorate --all"
-
+# alias git-tree="git log --pretty=oneline --graph --decorate --all"
+alias git-log='git log --oneline --decorate --graph'
+alias git-tree='git log --oneline --decorate --graph'
 
 
 # https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor#answer-36573710
 # Perform a dry run and see what will be removed
 alias git_clean_test_exclude_files_from_git_ignore="git clean -xdn"
 # Execute it
-alias git_clean_exclude_files_from_git_ignore="git clean -xdf"
+alias git_clean_xclude_files_from_git_ignore="git clean -xdf"
