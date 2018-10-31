905dea601773805ef0999559efa520ed5594bb17
Frank Xu <franky.xhl@gmail.com>
2018-10-31 22:12:58 +0900

    Add tar_compress

--- .zshrc.d/tar.sh
+++ .zshrc.d/tar.sh
@@ -4,3 +4,5 @@ alias tar_list='tar tf '
 alias tar_update='tar uPvf '
 alias tar_extract='tar xPvf '
 alias tar_package='tar -zcvf '
+# tar -czvf name-of-archive.tar.gz /path/to/directory-or-file
+alias tar_compress='tar -czvf'
