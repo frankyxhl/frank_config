9a963f21afa8a69283f62c099595d52dbf254d1e
Frank Xu <franky.xhl@gmail.com>
2019-01-09 14:53:57 +0900

    improve rsync

--- .zshrc.d/common.sh
+++ .zshrc.d/common.sh
@@ -60,7 +60,7 @@ alias ytb="youtube-dl $1 -f 37/22/35/34"
 alias swap-capslock-to-ctrl='setxkbmap -layout us -option ctrl:nocaps'
 
 # https://askubuntu.com/questions/17275/progress-and-speed-with-cp
-alias cpp="rsync -ah --progress"
+alias cpp="rsync --archive --human-readable --progress --partial"
 
 mvbk() {
     mv $1 $1.bak
