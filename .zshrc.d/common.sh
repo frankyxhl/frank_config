7c405f507a150eec9b8abafd38e11a1f0b55a05e
Frank Xu <franky.xhl@gmail.com>
2019-01-14 00:33:31 +0900

    Improve zshrc

--- .zshrc.d/common.sh
+++ .zshrc.d/common.sh
@@ -20,6 +20,7 @@ alias ld='ls -d */'
 # List dotfiles only
 alias l.='ls -d .*'
 
+# Make folder then change current path into it
 # https://unix.stackexchange.com/questions/125385/combined-mkdir-and-cd#answer-125386
 mc ()
 {
@@ -136,3 +137,46 @@ export EDITOR=vi
 # COMMAND # (wget -O - pi.dk/3 || curl pi.dk/3/) | bash
 alias p='parallel'
 alias pp='parallel --pipe -k'
+
+alias sc="vim ~/.ssh/config"
+
+# https://www.quora.com/What-is-the-most-useful-bash-script-that-you-have-ever-written
+weather(){
+    curl -s "wttr.in/$1?m1"
+}
+
+timer() {
+    total=$1
+    for ((i=total; i>0; i--)); do sleep 1; printf "Time remaining $i secs \r"; done
+    echo -e "\a"
+}
+
+up() {
+    times=$1
+    while [ "$times" -gt "0" ]; do
+        cd ..
+        times=$(($times - 1))
+    done
+}
+
+extract () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1    ;;
+            *.tar.gz)    tar xvzf $1    ;;
+            *.tar.xz)    tar Jxvf $1    ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       rar x $1       ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xvf $1     ;;
+            *.tbz2)      tar xvjf $1    ;;
+            *.tgz)       tar xvzf $1    ;;
+            *.zip)       unzip -d `echo $1 | sed 's/\(.*\)\.zip/\1/'` $1;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "don't know how to extract '$1'" ;;
+        esac
+    else
+        echo "'$1' is not a valid file."
+    fi
+}
