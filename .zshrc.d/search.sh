5587cf7a3b68fac3607d0eab980b9b27a9fdab0b
Frank Xu <franky.xhl@gmail.com>
2019-09-06 03:15:59 +0900

    Not used ff command

--- .zshrc.d/search.sh
+++ .zshrc.d/search.sh
@@ -22,20 +22,20 @@ find_lines_contains_string_in_current_folder_recursively() {
 }
 alias fl=find_lines_contains_string_in_current_folder_recursively
 
-find_files() {
-    if [ $# -eq 0 ] ; then
-        echo "Please input the file names you want to search."
-        echo "For example: find bar"
-        return
-    fi
-    if [ "$1" = "--help" ] ; then
-        echo "ff Find files"
-        echo "For example: find bar"
-        return
-    fi
-    find . -iname "*$1*" -type f -print
-}
-alias ff=find_files
+# find_files() {
+#     if [ $# -eq 0 ] ; then
+#         echo "Please input the file names you want to search."
+#         echo "For example: find bar"
+#         return
+#     fi
+#     if [ "$1" = "--help" ] ; then
+#         echo "ff Find files"
+#         echo "For example: find bar"
+#         return
+#     fi
+#     find . -iname "*$1*" -type f -print
+# }
+# alias ff=find_files
 
 remove_find_files(){
     find_files "$1"
